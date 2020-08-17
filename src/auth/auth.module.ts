3ec54e128f7be25a262b182d06f7ed8de282993d
beltrandy <beltrandy@outlook.com>
2020-08-17 00:34:25 -0700

    Imported angularfire modules and added Firebase config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 export const ROUTES: Routes = [];
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyAsHHTjlzCIRDzojJg9Em-jMnPcx-2sCS4",
+  authDomain: "hdts-app.firebaseapp.com",
+  databaseURL: "https://hdts-app.firebaseio.com",
+  projectId: "hdts-app",
+  storageBucket: "hdts-app.appspot.com",
+  messagingSenderId: "658584982023"
+};
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(ROUTES)
+    RouterModule.forChild(ROUTES),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ]
 })
 export class AuthModule {}
