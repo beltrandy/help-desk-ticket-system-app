75f1b0649b95c77fdae83723cf223b20e4f0db03
beltrandy <beltrandy@outlook.com>
2020-09-02 05:41:44 -0700

    Added auth-form to login component template

--- src/auth/login/containers/login/login.component.ts
+++ src/auth/login/containers/login/login.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'login',
+  selector: "login",
   template: `
     <div>
+      <auth-form>
+        <h1>Login</h1>
+        <button type="submit">Login</button>
+        <div class="error" *ngIf="error">
+          {{ error }}
+        </div>
+      </auth-form>
     </div>
-  `
+  `,
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  error: string;
+
+  constructor() {}
+}
