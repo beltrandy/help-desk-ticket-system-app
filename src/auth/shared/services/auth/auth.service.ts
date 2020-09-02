6ffa93b1d18f24dd9044e84af9fae29a744e8434
beltrandy <beltrandy@outlook.com>
2020-09-02 04:13:36 -0700

    Defined User interface within auth service

--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Store } from 'store';
+import { Store } from "store";
+
+export interface User {
+  email: string;
+  uid: string;
+  authenticated: boolean;
+}
 
 @Injectable()
 export class CallersService {
-
-    constructor() {}
+  constructor() {}
 }
