65df5ccee7c008a4cabeafb7579c6910d57db29e
beltrandy <beltrandy@outlook.com>
2020-09-07 05:45:29 -0700

    Implemented form functionality and added loginUser method to login component

--- src/auth/login/containers/login/login.component.ts
+++ src/auth/login/containers/login/login.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../shared/services/auth/auth.service';
 
 @Component({
   selector: "login",
   template: `
     <div>
-      <auth-form>
+      <auth-form (submitted)="loginUser($event)">
         <h1>Login</h1>
         <button type="submit">Login</button>
         <div class="error" *ngIf="error">
@@ -12,11 +16,24 @@ import { Component } from "@angular/core";
         </div>
       </auth-form>
     </div>
-  `,
+  `
 })
 export class LoginComponent {
 
   error: string;
 
-  constructor() {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async loginUser(event: FormGroup) {
+    const { email, password } = event.value;
+    try {
+      await this.authService.loginUser(email, password);
+      this.router.navigate(['/']);
+    } catch (err) {
+      this.error = err.message;
+    }
+  }
 }
