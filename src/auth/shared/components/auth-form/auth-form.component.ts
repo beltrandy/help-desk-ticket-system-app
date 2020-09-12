6cf1ffab217d9fb934eb73377c7d1642b96a8320
beltrandy <beltrandy@outlook.com>
2020-09-12 16:39:43 -0700

    Added auth-form template and defined FormGroup instance

--- src/auth/shared/components/auth-form/auth-form.component.ts
+++ src/auth/shared/components/auth-form/auth-form.component.ts
@@ -5,8 +5,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
     selector: 'auth-form',
     styleUrls: ['auth-form.component.scss'],
     template: `
-      <div>
+      <div class="auth-form">
+        <form [formGroup]="form" (ngSubmit)="onSubmit()">
+          
+          <ng-content select="h1"></ng-content>
+
+          <label>
+            <input 
+              type="email" 
+              placeholder="Email address"
+              formControlName="email">
+          </label>
+          <label>
+            <input 
+              type="password" 
+              placeholder="Enter password"
+              formControlName="password">
+          </label>
+
+          <div class="auth-form__action">
+            <ng-content select="button"></ng-content>
+          </div>
+
+        </form>
       </div>
     `
-  })
-  export class AuthFormComponent {}
+})
+export class AuthFormComponent {
+
+  @Output()
+  submitted = new EventEmitter<FormGroup>();
+
+  form = this.fb.group({
+    email: ['', Validators.email],
+    password: ['', Validators.required]
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.submitted.emit(this.form);
+    }
+  }
+  
+}
