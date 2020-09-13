6cd855ba8eb53945b31da73594ff9b4700a9030d
beltrandy <beltrandy@outlook.com>
2020-09-13 08:55:54 -0700

    Added styles, template, and logout method to header

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,8 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+
+import { User } from '../../../auth/shared/services/auth/auth.service';
 
 @Component({
   selector: "app-header",
   styleUrls: ["app-header.component.scss"],
-  template: ``,
+  template: `
+    <div class="app-header">
+      <div class="wrapper">
+        <div>
+          <img src="/img/hdts-logo.svg">
+          <div>Help Desk Ticket System</div>
+        </div>
+        <div 
+          class="app-header__user-info"
+          *ngIf="user?.authenticated">
+          <span (click)="logoutUser()"></span>
+        </div>
+      </div>
+    </div>
+  `
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent {
+
+  @Input()
+  user: User;
+
+  @Output()
+  logout = new EventEmitter<any>();
+  
+  logoutUser() {
+    this.logout.emit();
+  }
+  
+}
