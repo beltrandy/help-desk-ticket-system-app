3c0d2a75fcddf033cef747857f9a0ef40dd6596f
beltrandy <beltrandy@outlook.com>
2020-09-13 14:54:09 -0700

    Subscribing to auth observable and getting user from store in root component

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Store } from 'store';
+
+import { AuthService, User } from '../../../auth/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      HDTS
+      <app-header
+        [user]="user$ | async">
+      </app-header>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  user$: Observable<User>;
+  subscription: Subscription;
+
+  constructor(
+    private store: Store,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>('user');
+  }
+
 }
