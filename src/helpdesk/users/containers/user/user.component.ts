375ea414b6ea328819371174245ebc9dd49710b8
beltrandy <beltrandy@outlook.com>
2020-10-03 18:19:11 -0700

    Added user/users components

---
+++ src/helpdesk/users/containers/user/user.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user',
+    styleUrls: ['user.component.scss'],
+    template: `
+        <div>
+            
+        </div>
+    `
+})
+export class UserComponent {
+    constructor() {}
+}
---
+++ src/helpdesk/users/containers/users/users.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'users',
+    styleUrls: ['users.component.scss'],
+    template: `
+        <div>
+            
+        </div>
+    `
+})
+export class UsersComponent {
+    constructor() {}
+}
