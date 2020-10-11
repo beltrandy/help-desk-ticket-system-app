bc7cb45e1d021085a8872f826d6010c79e331ce3
beltrandy <beltrandy@outlook.com>
2020-10-11 00:57:31 -0700

    Created incidents service

--- src/helpdesk/shared/services/incidents/incidents.service.ts
+++ src/helpdesk/shared/services/incidents/incidents.service.ts
@@ -1,8 +1,55 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { AngularFireDatabase } from "angularfire2/database";
 
-export interface Incident {}
+import { Store } from "store";
+
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/map";
+import "rxjs/add/observable/of";
+
+export interface Incident {
+  caller: string;
+  agent: string;
+  priority: number;
+  status: string;
+  title: string;
+  description: string;
+  workNotes: string;
+  closeNotes: string;
+  $key: string;
+  $exists: () => boolean;
+}
 
 @Injectable()
 export class IncidentsService {
-    constructor() {}
+  incidents$: Observable<Incident[]> = this.db
+    .list(`incidents`)
+    .do((next) => this.store.set("incidents", next));
+
+  constructor(private store: Store, private db: AngularFireDatabase) {}
+
+  getIncident(key: string) {
+    if (!key) return Observable.of({});
+    return this.store
+      .select<Incident[]>("incidents")
+      .filter(Boolean)
+      .map((incidents) =>
+        incidents.find((incident: Incident) => incident.$key === key)
+      );
+  }
+
+  addIncident(incident: Incident) {
+    console.log("Incident:", incident);
+    return this.db.list(`incidents`).push(incident);
+  }
+
+  updateIncident(key: string, incident: Incident) {
+    return this.db.object(`incidents/${key}`).update(incident);
+  }
+
+  removeIncident(key: string) {
+    return this.db.list(`incidents`).remove(key);
+  }
 }
